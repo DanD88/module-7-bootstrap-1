e0ed29c83729a65cc3866cee68019a99162b8832
Daniel Davidson <DanDavidson88@gmail.com>
2019-02-19 12:52:42 -0800

    initial commit

--- page.php
+++ page.php
@@ -10,17 +10,33 @@
     <title>Page Title</title>
 </head>
 <body>
-<div class="container">
-    <section>
-        <h1>Evergreen</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
-        <h2>Camas</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
-        <h3>Vanmcouver</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+<div class="container mt-4">
+    <div>
+    <img class="img-fluid mb-4 d-block mx-auto" src="img/200px-Clark_College_logo.png" alt="">
+    <div class="alert alert-warning text-center font-weight-bold mt-4 p-4">
+            This page is a test!
+    </div class="row">
+    <div class="col-lg-12 col-sm-6">
+    <section class="card"> 
+        <div class="card-body">
+        <h1 class="card-body p-3 card-header bg-primary">Evergreen</h1>
+        <p class="lead px-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
+        </div>
+        <div class="card-body">
+        <h2 class="h1 card-header bg-danger">Camas</h2>
+        <p class="justify text-uppercase p-3">Lorem ipsum dolor sit a can you see me? met consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
+        </div>
+        <div class="card-body">
+        <h3 class="mt-3">Vancouver</h3>
+        <p class="mb-3 p-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+        </div>
+        <div class="card-body">
         <h4>Comsewogue</h4>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+        <p class="px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+        </div>
     </section>
+    </div>
+    </div>
 </div>
 
 <!-- jQuery -->
